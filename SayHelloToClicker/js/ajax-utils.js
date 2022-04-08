d67d125d59b1a5bd4c1e870d770c7cd3e6780600
IsuruFerna <69768677+IsuruFerna@users.noreply.github.com>
2022-04-08 14:21:13 +0200

    re write ajax

--- SayHelloToClicker/js/ajax-utils.js
+++ SayHelloToClicker/js/ajax-utils.js
@@ -1,38 +1,37 @@
-(function (global) {
-// Set up a namespace for utility
+(function(global){
+
+// Set up a namespace for our utility
 var ajaxUtils = {};
 
-// Return an HTTP request object
-function getRequestObject() {
-    if (window.XMLHttpRequest) {
+// Returns an HTTP request object
+function getRequestObject(){
+    if(window.XMLHttpRequest){
         return(new XMLHttpRequest());
-    } 
-    else if (window.ActiveXObject) {
-    // For very old IE browsers (optional)
-    return (new ActiveXObject("Microsoft.XMLHTTP"));
-}
-else {
-    global.alart("Ajax is not supported!")
-    return(null);
-}
+    }
+    else if(window.ActiveXObject) {
+        return (new ActiveXObject("Microsoft,XMLHTTP"));
+    }
+    else{
+        global.alert("Ajax is not supported!");
+        return (null);
+    }
 }
 
 // Makes an Ajax GET request to 'requestUrl'
 ajaxUtils.sendGetRequest = function(requestUrl, responseHandler){
     var request = getRequestObject();
     request.onreadystatechange = function(){
-        handleResponse(request,responseHandler);
+        handleResponse(request, responseHandler);
     };
-    request.open("GET",requestUrl, true);
-    request.send(null); // for POST only
+    request.open("GET", requestUrl, true);
+    request.send(null) // for post only
 };
 
 // Only calls user provided 'responseHandler'
 // function if response is ready
-// and not an error
-function handleResponse(request, responseHandler) {
-    if((request.readyState == 4) && (request.state == 200)) {
-        responseHandler(request)
+// and not error
+function handleResponse(request, responseHandler){
+    if((request.readyState == 4) && (request.status == 200)) {responseHandler(request);
     }
 }
 
