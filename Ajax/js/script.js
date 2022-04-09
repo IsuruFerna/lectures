ce6d8a9e2c311a1cfee29d0163b5336cbbe31635
IsuruFerna <69768677+IsuruFerna@users.noreply.github.com>
2022-04-09 22:09:04 +0200

    removed

--- Ajax/js/script.js
+++ Ajax/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function(event){
     .addEventListener("click", function(){
 
          // Call server to get the name
-         $ajaxUtils.sendGetRequest("/data/name.txt", function (request){
+         $ajaxUtils.sendGetRequest("data/name.txt", function (request){
              var name = request.responseText;
          document.querySelector("#content").innerHTML = "<h2>Hello " + name + "!</h2>";
          });
