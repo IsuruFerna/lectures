32dbddda3200ed780624934efb1ead44b7c53655
IsuruFerna <69768677+IsuruFerna@users.noreply.github.com>
2022-04-09 22:05:10 +0200

    must work now

--- Ajax/js/script.js
+++ Ajax/js/script.js
@@ -4,13 +4,11 @@ document.addEventListener("DOMContentLoaded", function(event){
     // unobstructive event binding
     document.querySelector("button")
     .addEventListener("click", function(){
-        var self = this;
-         var name = "";
 
          // Call server to get the name
          $ajaxUtils.sendGetRequest("/data/name.txt", function (request){
-             self.name = request.responseText;
+             var name = request.responseText;
+         document.querySelector("#content").innerHTML = "<h2>Hello " + name + "!</h2>";
          });
-         document.querySelector("#content").innerHTML = "<h2>Hello " + self.name + "!";
     });
-})
+});
