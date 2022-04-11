418c90b47f3ea31c7b606fe57c68a57661db1544
IsuruFerna <69768677+IsuruFerna@users.noreply.github.com>
2022-04-11 14:28:46 +0200

    script

--- Lecture59/Before/js/script.js
+++ Lecture59/Before/js/script.js
@@ -1,1 +1,14 @@
-hello
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+    
+    // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+    $("#navbarToggle").blur(function (event) {
+        var screenWidth = window.innerWidth;
+        if (screenWidth < 768) {
+            $("#collapsable-nav").collapse('hide')
+        }
+    });
+$("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+});
+
+});
